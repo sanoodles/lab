12c9d41c74130a48c246b588d00c76dc21ec2833
Samuel <samuelgomezcrespo@gmail.com>
2013-09-07 21:28:45 +0200

    Better output readability. Added observations about the output.

--- php/array_merge.php
+++ php/array_merge.php
@@ -22,7 +22,7 @@ $m1 = array(
 );
 $m2 = array(
 		1001 => array( // repeated key
-				"b11" => 221, // repeated key, different value
+				"b11" => 121, // repeated key, repeated value
 				"b12" => 222 // repeated key, different value
 		),
 		"asdf" => array( // repeated key
@@ -48,10 +48,18 @@ var_dump($merge);
 echo "\n\n\narray_merge(m1, m2)\n\n";
 $merge = array_merge($m1, $m2);
 var_dump($merge);
+echo "\n";
+echo "numeric keys are not preserved.\n";
+echo "numeric keys are not merged.\n";
+echo "string keys of the second dimension are overriden.\n";
 
 echo "\n\n\narray_merge_recursive(m1, m2)\n\n";
 $merge = array_merge_recursive($m1, $m2);
 var_dump($merge);
+echo "\n";
+echo "numeric keys are not preserved.\n";
+echo "numeric keys are not merged.\n";
+echo "string keys of the second dimension are merged.\n";
 
 echo "</pre>";
 
