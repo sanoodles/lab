13278c684ec0499638041004c1b93383a2c35420
Samuel <samuelgomezcrespo@gmail.com>
2013-09-17 07:15:12 +0200

    Array keys PHP

---
+++ php/array_keys.php
@@ -0,0 +1,50 @@
+<?php 
+
+$a1 = array("a", "b");
+$a2 = array("a", "b"); // same value, different reference
+$a3 = array("a", "c"); // different value (and different reference)
+$a4 = array("b", "a"); // different order
+
+$m1 = array();
+
+/*
+ * some arbitrary offset to ensure that this does not work just for the corner 
+ * case of the needle being at first position of the haystack
+ */  
+$m1[3] = array("asdf", "qwer");
+ 
+$m1[5] = $a1;
+
+
+
+echo "<pre>";
+
+echo "\n\n\n\n" . '$a1' . "\n\n";
+var_dump($a1);
+
+echo "\n\n\n\n" . '$m1' . "\n\n";
+var_dump($m1);
+
+echo "\n\n\n\n" . 'in_array($a1, "b")' . "\n\n";
+var_dump(array_keys($a1, "b"));
+echo "\nvanilla in_array";
+
+echo "\n\n\n\n" . 'in_array($m1, $a1)' . "\n\n";
+var_dump(array_keys($m1, $a1));
+echo "\ncan find same array in matrix";
+
+echo "\n\n\n\n" . 'in_array($m1, $a2)' . "\n\n";
+var_dump(array_keys($m1, $a2));
+echo "\ndifferent array with same values is evaluated as a hit";
+
+echo "\n\n\n\n" . 'in_array($m1, $a3)' . "\n\n";
+var_dump(array_keys($m1, $a3));
+echo "\ndifferent array with different values is evaluated as a miss";
+
+echo "\n\n\n\n" . 'in_array($m1, $a4)' . "\n\n";
+var_dump(array_keys($m1, $a4));
+echo "\ndifferent order of elements is evaluated as a miss";
+
+echo "</pre>";
+
+?>
