b16c6048a311b327c31aa0dd0e6b9a0ee9ea867f
Samuel <samuelgomezcrespo@gmail.com>
2013-10-05 22:52:44 +0200

    array_merge Perl WIP

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Table of contents:
 | Experiment          | JS        | Perl      | PHP       | Python    |
 | ------------------- |:---------:| ---------:| ---------:| ---------:|
 | Assignment val/ref  | YES       | YES       | YES       | YES       |
-| Array merge         | YES       |           | YES       | YES       |
+| Array merge         | YES       | WIP       | YES       | YES       |
 | Array keys          | YES       |           | YES       | YES       |
 | Explode             | YES       |           | YES       | YES       |
 
---
+++ perl/array_merge.pl
@@ -0,0 +1,107 @@
+use strict;
+use Data::Dumper;
+
+sub title
+{
+  my $v = shift;
+  print "\n\n\n\n$v\n\n";
+}
+
+sub sca_dump
+{
+  my $v = shift;
+  print Dumper $v;
+}
+
+sub arr_dump
+{
+  print "array {\n";
+  my @v = @_;
+  print Dumper @v;
+  print "}\n";
+}
+
+sub aar_dump
+{
+  print "associative array {\n";
+  my %v = @_;
+  print Dumper %v;
+  print "}\n";
+}
+
+sub observation
+{
+  my $v = shift;
+  print "\n$v";
+}
+
+
+
+my @a0 = ();
+my @a1 = ("red", "green");
+my @a2 = ("red", "blue"); # repeated element "red"
+
+my %m0 = ();
+
+# composition of hashes http://docstore.mik.ua/orelly/perl3/prog/ch09_04.htm
+my %m1 = (
+		1000 => {
+				"a11" => 111,
+				"a12" => 112
+    },
+    1001 => {
+				"b11" => 121,
+				"b12" => 122
+    },
+    "asdf" => {
+				"asdf11" => 131,
+				"asdf12" => 132
+    }
+);
+
+my %m2 = (
+		1001 => { # repeated key
+				"b11" => 121, # repeated key, repeated value
+				"b12" => 222 # repeated key, different value
+		},
+		"asdf" => { # repeated key
+				"asdf11" => 131, # repeated key, repeated value
+				"asdf12" => 232 # repeated key, different value
+    },
+		1002 => {
+				"c11" => 241,
+				"c12" => 242
+    }
+);
+
+
+
+
+print "Content-Type: text/html\n\n";
+print "<pre>";
+
+title("(a1, a2)");
+my @merge = (@a1, @a2); # array and hash merge http://docstore.mik.ua/orelly/perl3/prog/ch09_04.htm
+arr_dump(@merge);
+
+title("(a0, a1)");
+@merge = (@a0, @a1);
+arr_dump(@merge);
+
+title("(m1, m2)");
+my %merge = (%m1, %m2);
+aar_dump(%merge);
+observation("numeric keys become string keys.");
+observation("string keys of the second dimension are overwritten.");
+
+=pod
+echo "\n\n\narray_merge_recursive(m1, m2)\n\n";
+$merge = array_merge_recursive($m1, $m2);
+var_dump($merge);
+echo "\n";
+echo "numeric keys are NOT preserved.\n";
+echo "numeric keys are NOT merged.\n";
+echo "string keys of the second dimension are merged.\n";
+=cut
+print "</pre>";
+
