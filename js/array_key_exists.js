a86f059f92507cce2073c3bea9533b1096cdf4cb
Samuel <samuelgomezcrespo@gmail.com>
2013-11-22 20:39:15 +0100

    Array key exists for JS

---
+++ js/array_key_exists.js
@@ -0,0 +1,15 @@
+
+var m1 = {
+  "k1": "v1",
+  "k2": "v2"
+};
+
+console.log("-");
+console.log("m1");
+console.log(m1);
+
+console.log("-");
+console.log("m1.hasOwnProperty('k2')");
+console.log(m1.hasOwnProperty('k2'));
+console.log("vanilla hasOwnProperty");
+
