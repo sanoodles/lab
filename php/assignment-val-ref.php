980c686c3097828ca9564dfab2b60d19fdff7ca5
Samuel <samuelgomezcrespo@gmail.com>
2014-03-10 21:57:40 +0100

    Unit test assignment val ref PHP

--- php/assignment-val-ref.php
+++ php/assignment-val-ref.php
@@ -19,6 +19,7 @@ $arr1 = array($int1);
 var_dump($arr1);
 $int1 = 5;
 var_dump($int1);
+$res1 = $arr1;
 var_dump($arr1);
 observation("does not change <-> by value");
 
@@ -29,6 +30,7 @@ $arr1 = array($str1);
 var_dump($arr1);
 $str1 = "b";
 var_dump($str1);
+$res2 = $arr1;
 var_dump($arr1);
 observation("does not change <-> by value");
 
@@ -39,6 +41,7 @@ $arr1 = array($arr2);
 var_dump($arr1);
 $arr2[] = "b";
 var_dump($arr2);
+$res3 = $arr1;
 var_dump($arr1);
 observation("does not change <-> by value");
 
@@ -49,6 +52,7 @@ $arr1 = array($map1);
 var_dump($arr1);
 $map1["k1"] = "b";
 var_dump($map1);
+$res4 = $arr1;
 var_dump($arr1);
 observation("does not change <-> by value");
 
@@ -74,6 +78,7 @@ $arr1 = array($obj1);
 var_dump($arr1);
 $obj1 = new C1("b");
 var_dump($obj1);
+$res5 = $arr1;
 var_dump($arr1);
 observation("does not change");
 
@@ -84,9 +89,51 @@ $arr1 = array($obj1);
 var_dump($arr1);
 $obj1->setA1("b");
 var_dump($obj1);
+$res6 = $arr1;
 var_dump($arr1);
 observation("changes");
 
 echo "</pre>";
+
+if (!class_exists('PHPUnit_Framework_TestCase')) exit;
+
+class Test extends PHPUnit_Framework_TestCase
+{
+  public function test1()
+  {
+    global $res1;
+    $this->assertEquals([3], $res1);
+  }
+
+  public function test2()
+  {
+    global $res2;
+    $this->assertEquals(['a'], $res2);
+  }
+
+  public function test3()
+  {
+    global $res3;
+    $this->assertEquals([['a']], $res3);
+  }
+
+  public function test4()
+  {
+    global $res4;
+    $this->assertEquals([['k1' => 'a']], $res4);
+  }
+
+  public function test5()
+  {
+    global $res5;
+    $this->assertEquals([new C1('a')], $res5);
+  }
+
+  public function test6()
+  {
+    global $res6;
+    $this->assertEquals([new C1('b')], $res6);
+  }
+}
 
 ?>
