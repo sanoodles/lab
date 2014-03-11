d31a878c721c3c906580612b641f3cbec3be6219
Samuel <samuelgomezcrespo@gmail.com>
2014-03-11 22:09:52 +0100

    Unit tests for PHP explode.

--- php/explode.php
+++ php/explode.php
@@ -19,30 +19,65 @@ $s4 = "asdf";
 echo "<pre>";
 
 title('vanilla explode');
-$res = explode(",", $s1);
-var_export($res);
+$res1 = explode(",", $s1);
+var_export($res1);
 observation("");
 
 title('explode empty string');
-$res = explode(",", $s0);
-var_export($res);
+$res2 = explode(",", $s0);
+var_export($res2);
 observation("one position, with an empty string is returned.");
 
 title('explode on delimiter at the end');
-$res = explode(",", $s2);
-var_export($res);
+$res3 = explode(",", $s2);
+var_export($res3);
 observation("one ending position, with an empty string, is returned.");
 
 title('explode on delimiter at the beginning');
-$res = explode(",", $s3);
-var_export($res);
+$res4 = explode(",", $s3);
+var_export($res4);
 observation("");
 
 title('explode on no delimiter');
-$res = explode(",", $s4);
-var_export($res);
+$res5 = explode(",", $s4);
+var_export($res5);
 observation("");
 
 echo "</pre>";
+
+if (!class_exists('PHPUnit_Framework_TestCase')) exit;
+
+class Test extends PHPUnit_Framework_TestCase
+{
+  public function test1()
+  {
+    global $res1;
+    $this->assertEquals(['asdf', 'qwer'], $res1);
+  }
+
+  public function test2()
+  {
+    global $res2;
+    $this->assertEquals([''], $res2);
+  }
+
+  public function test3()
+  {
+    global $res3;
+    $this->assertEquals(['asdf', ''], $res3);
+  }
+
+  public function test4()
+  {
+    global $res4;
+    $this->assertEquals(['', 'qwer'], $res4);
+  }
+
+  public function test5()
+  {
+    global $res5;
+    $this->assertEquals(['asdf'], $res5);
+  }
+}
 
 ?>
