3e8f03714b44a6d12384a0b439824ec72aad255f
Samuel <samuelgomezcrespo@gmail.com>
2014-05-24 17:26:03 +0200

    Refactor header and footer into separate files. Exclude xhprof built-in functions from the profiling.

---
+++ php/profiling/page-header.php
@@ -0,0 +1,6 @@
+<?php
+xhprof_enable(
+    XHPROF_FLAGS_CPU + 
+    XHPROF_FLAGS_MEMORY +
+    XHPROF_FLAGS_NO_BUILTINS);
+?>
--- php/profiling/profiling-no-db.php
+++ php/profiling/profiling-no-db.php
@@ -1,5 +1,5 @@
 <?php
-xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+require "page-header.php";
 
 function light_loop() {
   for ($i = 0; $i <= 10 * 1000; $i++) {
@@ -24,16 +24,6 @@ function top_tier() {
 
 top_tier();
 
-$xhprof_data = xhprof_disable();
-
-$XHPROF_ROOT = "/usr/share/php/xhprof_lib";
-include_once $XHPROF_ROOT . "/utils/xhprof_lib.php";
-include_once $XHPROF_ROOT . "/utils/xhprof_runs.php";
-
-$xhprof_runs = new XHProfRuns_Default();
-$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
-
-echo '<a href="http://localhost/php/profiling/xhprof_html/index.php?run=' . $run_id . '&source=xhprof_testing">Results</a>';
-
+require "page-footer.php";
 ?>
 
