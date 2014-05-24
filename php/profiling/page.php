63711efeea077a2c44b9e13ca915a1aeab01b609
Samuel <samuelgomezcrespo@gmail.com>
2014-05-24 17:51:03 +0200

    Add file for page-level symbols.

---
+++ php/profiling/page.php
@@ -0,0 +1,22 @@
+<?php
+function page_head() {
+  xhprof_enable(
+      XHPROF_FLAGS_CPU + 
+      XHPROF_FLAGS_MEMORY +
+      XHPROF_FLAGS_NO_BUILTINS);
+}
+
+function page_footer($profiling_name) {
+  $xhprof_data = xhprof_disable();
+
+  $XHPROF_ROOT = "/usr/share/php/xhprof_lib";
+  include_once $XHPROF_ROOT . "/utils/xhprof_lib.php";
+  include_once $XHPROF_ROOT . "/utils/xhprof_runs.php";
+
+  $xhprof_runs = new XHProfRuns_Default();
+  $run_id = $xhprof_runs->save_run($xhprof_data, $profiling_name);
+
+  echo '<a href="http://localhost/php/profiling/xhprof_html/index.php?run=' . $run_id . '&source=' . $profiling_name . '">Results</a>';
+}
+
+?>
