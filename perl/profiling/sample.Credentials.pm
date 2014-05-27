6db367fe2ac3f6a4c446e7f39fe5a29d0b2921aa
Samuel <samuelgomezcrespo@gmail.com>
2014-05-27 21:16:53 +0200

    Added sample Credentials package.

---
+++ perl/profiling/sample.Credentials.pm
@@ -0,0 +1,13 @@
+package Credentials;
+
+use strict;
+use warnings;
+
+use Exporter::Shiny qw(DB_HOST DB_NAME DB_USER DB_PASS);
+
+use constant DB_HOST => "foo";
+use constant DB_NAME => "bar";
+use constant DB_USER => "baz";
+use constant DB_PASS => "qux";
+
+1;
