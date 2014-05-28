408e0ede3e4ca4e7b47ff63747bc2d7b1f35da77
Samuel <samuelgomezcrespo@gmail.com>
2014-05-28 22:38:46 +0200

    Profiling DB for Python

---
+++ python/profiling/profiling-db.py
@@ -0,0 +1,43 @@
+import psycopg2
+
+import credentials
+
+def light_query():
+    st = """
+        SELECT *
+        FROM table1, table2
+    """
+    cur.execute(st)
+    rows = cur.fetchall()
+    for row in rows:
+        pass
+
+def heavy_query():
+    st = """
+        SELECT *
+        FROM table1, table2
+        ORDER BY table1.field1
+    """
+    cur.execute(st)
+    rows = cur.fetchall()
+    for row in rows:
+        pass
+
+def mid_tier():
+    light_query()
+    heavy_query()
+
+def top_tier():
+    mid_tier()
+
+conn = psycopg2.connect(
+        database=credentials.DB_NAME, 
+        user=credentials.DB_USER,
+        host=credentials.DB_HOST,
+        password=credentials.DB_PASS
+)
+
+cur = conn.cursor()
+
+top_tier()
+
---
+++ python/profiling/sample.credentials.py
@@ -0,0 +1,4 @@
+DB_HOST = "foo"
+DB_NAME = "bar"
+DB_USER = "baz"
+DB_PASS = "qux"
