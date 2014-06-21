bcc2affe2f325ef813e30f9771b9f305521267cc
Samuel <samuelgomezcrespo@gmail.com>
2014-06-21 19:21:09 +0200

    Add the no-db running code.

---
+++ js/profiling/profiling-no-db.js
@@ -0,0 +1,26 @@
+function light_loop() {
+  var a = 0;
+  for (var i = 0; i < 10 * 1000 * 1000; i++) {
+    a = i * i;
+  }
+}
+
+function heavy_loop() {
+  var a = 0;
+  for (var i = 0; i < 100 * 1000 * 1000; i++) {
+    a = i * i;
+  }
+}
+
+function mid_tier() {
+  light_loop();
+  heavy_loop();
+}
+
+function top_tier() {
+  mid_tier();
+}
+
+
+setTimeout(top_tier(), 3000);
+
